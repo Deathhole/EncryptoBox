0bf3eab9d9819791c746b8ff8d2736c718dfdfff
Deathhole <pruthviprakash17@gmail.com>
2025-05-10 20:19:08 +0530

    new

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE: string;
-  readonly VITE_FIREBASE_API_KEY: string;
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-  readonly VITE_FIREBASE_PROJECT_ID: string;
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_API_BASE: "http://localhost:8000";
+  readonly VITE_FIREBASE_API_KEY: "AIzaSyBd7GNaFzfpX3euKSa3ahLJnI-dOxV0oHA";
+  readonly VITE_FIREBASE_AUTH_DOMAIN: "enceyptobox.firebaseapp.com";
+  readonly VITE_FIREBASE_PROJECT_ID: "enceyptobox";
+  readonly VITE_FIREBASE_STORAGE_BUCKET: "enceyptobox.firebasestorage.app";
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: "430443839084";
+  readonly VITE_FIREBASE_APP_ID: "1:430443839084:web:b40c104e9fc562b0e0e249";
 }
 
 interface ImportMeta {
