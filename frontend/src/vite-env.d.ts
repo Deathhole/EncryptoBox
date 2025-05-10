64b717daca406841c811c7621ee72049231523e9
Deathhole <pruthviprakash17@gmail.com>
2025-05-10 20:12:13 +0530

    new

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,8 +2,12 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE: string;
-  // add more environment variables here as needed, e.g.:
-  // readonly VITE_ANOTHER_KEY: string;
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
